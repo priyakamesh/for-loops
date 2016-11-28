51587ac51560322d552e03f0e592c214fe56f33d
krishnapriya <krishnapriyakamesh@gmail.com>
2016-11-28 13:22:43 -0600

    added index.html and for-loops.js file with all excercises done

---
+++ for-loops.js
@@ -0,0 +1,30 @@
+for (var i = 5; i < 120; i+=10) {
+	console.log(i);
+}
+
+for (var i = 4096; i >= 1; i = i/2) {
+	console.log(i);
+}
+
+var president = ["George Washington", "John Adams", "Thomas Jefferson"]
+ console.log (president);
+ for (var i = 0; i < president.length; i++) {
+ 	console.log("President#" + i + " was " + president[i]);
+ }
+
+ var antSpecies = {
+  argentine: {},
+  army: {},
+  bigHeaded: {},
+  black: {},
+  bull: {},
+  carpenter: {},
+  crazy: {},
+  fire: {},
+  glider: {},
+  honeyPot: {},
+  jackJumper: {}
+}
+ for (var prop in antSpecies) {
+ 	console.log (prop);
+ }
